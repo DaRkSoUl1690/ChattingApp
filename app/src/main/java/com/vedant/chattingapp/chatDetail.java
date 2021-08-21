491920a10314ed88db80d98c92250f137be494b3
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-08-21 17:33:36 +0530

    second Commit

--- app/src/main/java/com/vedant/chattingapp/chatDetail.java
+++ app/src/main/java/com/vedant/chattingapp/chatDetail.java
@@ -1,19 +1,32 @@
 package com.vedant.chattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+import com.vedant.chattingapp.Adapters.chatAdapter;
+import com.vedant.chattingapp.Models.MessageModel;
 import com.vedant.chattingapp.databinding.ActivityChatDetailBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class chatDetail extends AppCompatActivity {
 
-    ActivityChatDetailBinding binding;
+   ActivityChatDetailBinding binding;
     FirebaseDatabase database;
     FirebaseAuth auth;
     @Override
@@ -26,7 +39,7 @@ public class chatDetail extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
                auth = FirebaseAuth.getInstance();
 
-        String senderId = auth.getUid();
+      final   String senderId = auth.getUid();
         String recieveid = getIntent().getStringExtra("userId");
         String userName = getIntent().getStringExtra("username");
         String profilepic = getIntent().getStringExtra("profilepic");
@@ -40,5 +53,73 @@ public class chatDetail extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final chatAdapter chatAdapter = new chatAdapter(messageModels , this);
+        binding.recyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager  = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(layoutManager);
+
+        final String sendeRoom = senderId + recieveid;
+        final String recieverRoom = recieveid + senderId;
+
+database.getReference().child("Chats")
+        .child(sendeRoom)
+
+       .addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+
+        messageModels.clear();
+        for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
+        {
+            MessageModel model  = dataSnapshot1.getValue(MessageModel.class);
+
+            messageModels.add(model);
+        }
+        chatAdapter.notifyDataSetChanged();
+
+    }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError error) {
+
+           }
+
+
+       });
+
+
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String message =   binding.messagebar.getText().toString();
+              final MessageModel messageModel = new MessageModel(senderId,message);
+              messageModel.setTimestamp(new Date().getTime());
+              binding.messagebar.setText(" ");
+
+              database.getReference().child("Chats")
+                      .child(sendeRoom)
+                      .push()
+                      .setValue(messageModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+                  @Override
+                  public void onSuccess(Void unused) {
+                      database.getReference().child("Chats")
+                              .child(recieverRoom)
+                              .push()
+                              .setValue(messageModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+                          @Override
+                          public void onSuccess(Void unused) {
+
+                          }
+                      });
+                  }
+              });
+            }
+        });
+
     }
 }
